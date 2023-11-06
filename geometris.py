0a7109f67239d4840b61d03c276d775d4f6ca34d
Dannielle Villanueva <6874781+Emenopi@users.noreply.github.com>
2023-11-06 10:17:36 +0000

    Add init game matrix fn

--- geometris.py
+++ geometris.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 pygame.init()
 
@@ -15,13 +16,24 @@ circleRect = circle.get_rect()
 BORDER = (SCREEN_DIM-CIRCLE_DIM)/2
 CENTRE = SCREEN_DIM/2
 INTERNAL_RADIUS = (CIRCLE_DIM/2)*0.88
+CENTRE_RADIUS = math.floor(INTERNAL_RADIUS/10)
+if INTERNAL_RADIUS % 10 != 0:
+    CENTRE_RADIUS += INTERNAL_RADIUS % 10
 
-def gridMatrix (radius):
+def gridMatrix ():
     #init 60x? grid, determine ? based on how many will fit in radius
+    matrix = []
+    matrixHeight = math.floor((INTERNAL_RADIUS - CENTRE_RADIUS)/40)
+    for i in range(matrixHeight):
+        GAME_MATRIX.append([])
+        for j in range(60):
+            GAME_MATRIX[i].append(0)
+    return matrix
 
-def playCircle ():
+
+#def playCircle ():
     #game loop for circle tetris
-    
+
 play = True
 
 while play:
@@ -35,7 +47,9 @@ while play:
     screen.fill((000, 000, 000))
 
     screen.blit(circle, (BORDER, BORDER))
-    pygame.draw.circle(screen, (0, 0, 255), (CENTRE, CENTRE), INTERNAL_RADIUS)
+    pygame.draw.circle(screen, (195, 33, 45), (CENTRE, CENTRE), CENTRE_RADIUS)
+
+    GAME_MATRIX = gridMatrix()
 
     pygame.display.flip()
 
